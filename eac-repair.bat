898c8555104bdd67d286bf413345f0893b8069cb
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-05-04 07:14:30 +0200

    Update eac-repair.bat

--- eac-repair.bat
+++ eac-repair.bat
@@ -1,26 +1,20 @@
 @echo off
 
-@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" >nul 2>&1
+@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -y" >nul 2>&1
 SET "PATH=%PATH%;C:\ProgramData\chocolatey\bin"
-
 
 choco install python -y
 
-
-choco install git -y
+choco install wget -y
 
 curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
 
 python get-pip.py
 
-
 git clone https://github.com/s-b-repo/easy-peasy-ssh.git
-
 
 cd easy-peasy-ssh
 
-
 pip install pywin32
-
 
 python Java-subprocess-manager.py
